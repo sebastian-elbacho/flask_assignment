ddef96e2434c352c21d29c4a7103de4e66bbb449
Sebastian <sebastian.bachowski@gmail.com>
2025-08-01 09:59:49 +0100

    coments added

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+#We import the necessary modules from Flask
 from flask import Flask, render_template, request
 
+# We create an instance of the Flask application
 app = Flask(__name__)
 
 # The main route - website
@@ -8,17 +10,24 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+# Route to the projects section
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
+
+# route to the skills section
 
 @app.route('/skills')
 def skills():
     return render_template('skills.html')
 
+#route to the about section
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+# route to the contact form  (GET – shows the form, POST – receives data).
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
@@ -26,11 +35,17 @@ def contact():
         name = request.form['name']
         email = request.form['email']
         message = request.form['message']
+
+        # Printing a message to the console (simulating sending a message)
         print(f"Odebrano wiadomość od {name} ({email}): {message}")
+
+        # We pass data from the form to the template
         return render_template('contact.html', submitted=True, name=name)
+    
+     # Dla metody GET zwracamy pusty formularz
     return render_template('contact.html', submitted=False)
    
-
+# We only run the application if the file is run directly
 if __name__ == '__main__':
     app.run(debug=True)
 
